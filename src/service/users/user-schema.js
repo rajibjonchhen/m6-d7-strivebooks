4f2a3883334e3048e0fb57abbb19a0d74dfc06c3
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-02-15 12:52:01 +0000

    schema created

---
+++ src/service/users/user-schema.js
@@ -0,0 +1,9 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    "":"",
+    "":"",
+    "":""
+})
+
+export default model("Users", userSchema )
--- src/service/users/users.js
+++ src/service/users/users.js
@@ -1,5 +1,67 @@
 import express, {Router} from "express";
+import createError from "http-errors";
 
 const usersRouter = Router()
+
+/************************* post new *********************************/ 
+usersRouter.post("/", async(req, res, next) => {try {
+    
+} catch (error) {
+    next(error)
+}})
+
+
+/*************************** get all the *******************************/ 
+usersRouter.get("/", async(req, res, next) => {try {
+    if(true){
+
+    }else{
+        next(createError(404, "could not find the specific "))
+    }
+} catch (error) {
+    next(error)
+}})
+
+
+/****************************** get specific ****************************/ 
+
+usersRouter.get("/:id", async(req, res, next) => {try {
+    
+    if(true){
+
+    }else{
+        next(createError(404, "could not find the specific "))
+    }
+} catch (error) {
+    next(error)
+}})
+
+
+/***************************** update specific *****************************/ 
+
+usersRouter.put("/:id", async(req, res, next) => {try {
+    
+    if(true){
+
+    }else{
+        next(createError(404, "could not find the specific "))
+    }
+} catch (error) {
+    next(error)
+}})
+
+
+/**************************** delete specific ******************************/ 
+
+usersRouter.delete("/:id", async(req, res, next) => {try {
+    
+    if(true){
+
+    }else{
+        next(createError(404, "could not find the specific "))
+    }
+} catch (error) {
+    next(error)
+}})
 
 export default usersRouter
