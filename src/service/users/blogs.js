2f721a786c03e9dffbca3f39cfee79f3117a30ff
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-02-16 09:55:35 +0000

    minor changes

--- src/service/users/blogs.js
+++ src/service/users/blogs.js
@@ -8,7 +8,7 @@ blogsRouter.post("/", async(req, res, next) =>
 {try {
     const newBlog = new blogsModel(req.body)
     const {_id} = await newBlog.save()
-    res.status(201).send(_id)
+    res.status(201).send({_id:_id})
 } catch (error) {
     next(error)
 }})
